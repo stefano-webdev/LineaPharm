a05c635a976ddc0c75a5292ccc44bb1760eb4454
Stefano Quaranta <stefanooquaranta@gmail.com>
2026-01-26 22:40:48 +0100

    Add touch-active.js to enable :active styles on iOS devices

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 add_action('wp_enqueue_scripts', function() {
+    // Enqueue the child theme's stylesheet
     wp_enqueue_style('twentytwentyfive-child-style', get_stylesheet_uri());
+
+    // Enqueue touch-active.js to enable :active styles on iOS devices
+    wp_enqueue_script('twentytwentyfive-child-touch-active', get_stylesheet_directory_uri() . '/js/touch-active.js', [], false, true);
+
+    // Conditionally enqueue scripts for specific pages
     if (is_front_page()) {
         wp_enqueue_script('twentytwentyfive-child-home-script', get_stylesheet_directory_uri() . '/js/home.js', [], false, true);
     }
---
+++ js/touch-active.js
@@ -0,0 +1,2 @@
+// Need to add this empty listener to ensure :active CSS styles are applied on iOS devices
+document.addEventListener("touchstart", function() {}, true);
